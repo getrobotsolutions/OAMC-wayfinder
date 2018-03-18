770387703aad3b54d2b31dec96be29284d66d57b
sathapa <sathapa@my.bridgeport.edu>
2018-03-17 20:56:48 -0400

    Todo lust Update 2
    1. banner dots removed
    2. Home icon increased
    3. Desc modify on eat and shop
    4. Cursor changed
    5. Scanner js changed

--- Contents/BoardingPass/js/scanner.js
+++ Contents/BoardingPass/js/scanner.js
@@ -229,10 +229,9 @@ console.log(parts_p);
 
                       }   
 
-                      else{
-                      //alert("Please put the Barcode scanner on the right position and try again.");
+                      /*else{
                         location.reload();
-                      }                     
+                      }*/                     
               });
                  
                     setTimeout(function() {
